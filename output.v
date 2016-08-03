cf093d95ea813db25c9fb986ca9e5fd57438fe89
RENESYS <hrs350x@gmail.com>
2016-08-03 15:46:39 -0700

    add labeling function

--- output.v
+++ output.v
@@ -1,8 +1,9 @@
-module FSM(reset, clk, i3, i4, i1, i2, out);
-	input reset, clk, i3, i4, i1, i2;
+module FSM(reset, clk, i2, i4, i3, i1, out);
+	input reset, clk, i2, i4, i3, i1;
 	output reg[16:0] out;
 	parameter s1 = 0, s2 = 200, s3 = 700, s4 = 900, s5 = 1300, s6 = 1800, s7 = 2300, s8 = 2800, s9 = 3100, s10 = 3400, s11 = 3600, s12 = 3800, s13 = 4100;
 	reg[16:0] state, nextState;
+	reg[128:0] label;
 
 	always @(*) begin
 		if(reset) begin
@@ -13,15 +14,17 @@ module FSM(reset, clk, i3, i4, i1, i2, out);
 		end
 	end
 
-	always @(i3, i4, i1, i2, clk) begin
+	always @(i2, i4, i3, i1, clk) begin
 		case(state)
 		s1 : begin
 			out <= s1;
+			label <= 1'bx;
 			if(i3 == 1) nextState <= s2;
 			else nextState <= s1;
 		end
 		s2 : begin
 			out <= s2;
+			label <= "repeat";
 			if(i1 == 1 && i4 == 1) nextState <= s3;
 			else nextState <= s1;
 		end
@@ -32,46 +35,51 @@ module FSM(reset, clk, i3, i4, i1, i2, out);
 		end
 		s4 : begin
 			out <= s4;
-			if(i1 == 0 && i3 == 1) nextState <= s5;
-			else nextState <= s1;
+			label <= 1'bx;
+			if(i4 == 0 && i2 == 1 && i1 == 0) nextState <= s5;
+			else nextState <=s4;
 		end
 		s5 : begin
 			out <= s5;
-			if(i2 == 1 && i1 == 0 && i4 == 0) nextState <= s6;
+			if(i4 == 1 && i2 == 1 && i1 == 1 && i3 == 1) nextState <= s6;
 			else nextState <= s1;
 		end
 		s6 : begin
 			out <= s6;
-			if(i1 == 1) nextState <= s7;
-			else nextState <=s6;
+			if(i3 == 1) nextState <= s7;
+			else nextState <= s1;
 		end
 		s7 : begin
 			out <= s7;
-			if(i4 == 1) nextState <= s8;
+			label <= "repeat";
+			if(i1 == 1 && i4 == 1) nextState <= s8;
 			else nextState <= s1;
 		end
 		s8 : begin
 			out <= s8;
 			if(i4 == 0 && i3 == 0) nextState <= s9;
-			else nextState <= s1;
+			else nextState <=s8;
 		end
 		s9 : begin
 			out <= s9;
-			if(i4 == 1 && i1 == 0) nextState <= s10;
+			label <= 1'bx;
+			if(i3 == 1) nextState <= s10;
 			else nextState <= s1;
 		end
 		s10 : begin
 			out <= s10;
+			label <= "repeat";
 			if(i2 == 0 && i3 == 1) nextState <= s11;
 			else nextState <= s1;
 		end
 		s11 : begin
 			out <= s11;
-			if(i1 == 1 && i4 == 0) nextState <= s12;
+			if(i1 == 1 && i4 == 1) nextState <= s12;
 			else nextState <= s1;
 		end
 		s12 : begin
 			out <= s12;
+			label <= 1'bx;
 			if(i3 == 0) nextState <= s13;
 			else nextState <= s1;
 		end
