44747813f76afc4e259dd2cfa9d8e93c2ab900fa
blackpad\blacked <think>
2016-08-10 15:50:54 -0700

    modify FSM class and setFSM() method

--- fsm_i2c.py
+++ fsm_i2c.py
@@ -8,18 +8,24 @@ class FSM:
 
     # print the header part of verilog file
     def printHeader(self, output):
-        output.write("module " + self.s_moduleName + "(reset, clk, " + self.makeInputVarString() + ", out);\n")
-        output.write("\tinput reset, clk, " + self.makeInputVarString() + ";\n")
+        output.write("module " + self.s_moduleName + "(reset, " + self.makeInputVarString() + ", out);\n")
+        output.write("\tinput reset, " + self.makeInputVarString() + ";\n")
         output.write("\toutput reg[64:0] out;\n")
         output.write("\tparameter " + self.makeStateString() + ";\n")
-        output.write("\treg[64:0] state, nextState;\n")  # change 1 bit into 16 bits
-        output.write("\treg[128:0] label;\n\n")  # to label repeated states
+        output.write("\treg[64:0] state, nextState;\n") # change 1 bit into 16 bits
+        output.write("\treg[128:0] label;\n\n") # to label repeated states
 
     # print the initialize part of verilog file
     def printInitialize(self, output):
         string = '\t' + 'always @(*) begin\n'
         string += '\t\t' + 'if(reset) begin\n'
         string += '\t\t\t' + 'state <= ' + self.li_states[0].s_name + ';\n'
+        """
+        string += '\t\t\t'
+        for v in self.dic_inputVal.values():
+            string = string + v + ' = 0, '
+        string = string[:-2]
+        """
         string += '\t\t' + 'end\n\t\t' + 'else begin\n'
         string += '\t\t\t' + 'state <= nextState;\n\t\t' + 'end\n\t'
         string += 'end\n\n'
@@ -27,7 +33,7 @@ class FSM:
 
     # print the state changes & transition part of verilog file
     def printTransition(self, output):
-        output.write('\t' + 'always @(' + self.makeInputVarString() + ', clk) begin\n\t\t' + 'case(state)\n')
+        output.write('\t' + 'always @(' + self.makeInputVarString() + ') begin\n\t\t' + 'case(state)\n')
         for i in range(0, len(self.li_states)):
             curState = self.li_states[i]
             if i == len(self.li_states) - 1:
@@ -41,13 +47,13 @@ class FSM:
                     # default state
                     if self.li_states[i].s_name == 's1':
                         string += "\t\t\tlabel <= 1'bx;\n"
-
-                elif self.li_states[i].s_labeled == 0:  # stop condition
+                        
+                elif self.li_states[i].s_labeled == 0: # stop condition
                     string += "\t\t\tlabel <= 1'bx;\n"
-
+                    
                 elif self.li_states[i].s_labeled == 1:
                     string += '\t\t\tlabel <= "repeat";\n'
-
+                    
                 string += '\t\t\tif('
                 # if phrase
                 for trans in curState.li_transitions:
@@ -56,6 +62,7 @@ class FSM:
                 string = string[:-4]
 
                 string += ') nextState <= ' + self.li_states[i + 1].s_name + ';\n'
+                
                 if self.li_states[i + 1].b_isLoop == 0:
                     string += "\t\t\telse nextState <= s1;\n"
                 else:
@@ -77,10 +84,10 @@ class FSM:
     # make the state string (e.g. s1 = 0, s2 = 10, s3 = 30)
     def makeStateString(self):
         s = ""
-        idx = 1
+        index = 1
         for state in self.li_states:
-            s += (state.s_name + " = \"s" + str(idx) + "\", ")
-            idx += 1
+            s += (state.s_name + ' = "s' + str(index) + '", ')
+            index = index + 1
         s = s[:-2]
         return s
 
@@ -97,13 +104,64 @@ class FSM:
     def getInputVal(self, k):
         return self.dic_inputVal[k]
 
+    # set FSM class
+    def setFSM(self, lines):
+        idx = 1
+        stat = 0
+        loopStat = 0
+        beforeStat = 0
+        isVarSetting = 0
+        tempdic = {}
+    
+        for line in lines:
+            if line[0] == '$':
+                line = line[1:]
+                words = line.split()
+
+                # read module name and original value names at .vcd and write on the FSM class
+                if words[0] == 'scope':
+                    self.setModuleName(words[2])
+                elif words[0] == 'var' and words[1] == 'reg':
+                    print(words[4], "is assigned to", words[3])
+                    self.setInputValue(words[3], words[4])
+                else:
+                    """nothing"""
+            # set first state(s1) information
+            elif line[0] == '#':
+                if isVarSetting == 1:
+                    beforeStat.setTransition(tempdic, "s" + str(idx - 1))
+                    self.setState(beforeStat)
+                    isVarSetting = 0
+                tempdic = {}
+                time = line[1:-1]
+                beforeStat = stat
+                stat = State("s" + str(idx), time)
+
+                idx += 1
+                if time != "0":
+                    isVarSetting = 1
+
+            elif line == "...\n":
+                stat.setIsLoop()
+                continue
+                
+            elif isVarSetting == 1:
+                # go to the next line and set the transitions information            
+                value = line[0]
+                var = fsm.getInputVal(line[1])
+                tempdic[var] = value
+
+        beforeStat.setTransition(tempdic, "s" + str(idx - 1))
+        self.setState(beforeStat)
+
+        self.setState(stat)
 
 class State:
     s_name = ""
     i_value = 0
     li_transitions = []
     b_isLoop = 0
-    s_labeled = -1  # default value to detect whether current state is start or stop
+    s_labeled = -1 # default value to detect whether current state is start or stop
 
     def __init__(self, n, t):
         self.s_name = n
@@ -117,12 +175,6 @@ class State:
     def setIsLoop(self):
         self.b_isLoop = 1
 
-    def setStartLabeled(self):
-        self.s_labeled = 1
-
-    def setStopLabeled(self):
-        self.s_labeled = 0
-
     def printState(self):
         print("STATE : " + self.s_name + "\t\tOccurrence time : " + self.i_value + "ps")
         for t in self.li_transitions:
@@ -130,7 +182,7 @@ class State:
         if len(self.li_transitions) == 0:
             print("\tThis is the end state\n")
         else:
-            print("\telse if neither variables change")
+            print("\telse if no variable is changed")
             print("\t\t--> Next State is " + self.s_name)
             print("\telse Next state is s1\n")
 
@@ -148,67 +200,9 @@ class Transition:
     def printTransitionInfo(self):
         string = "\tif "
         for k in self.dic_tranValue.keys():
-            string += k + " changes to " + self.dic_tranValue[k] + " and "
+            string += k + " is changed to " + self.dic_tranValue[k] + " and "
         string = string[:-4]
         print(string + "\n\t\t--> Next State is " + self.s_dest)
-
-
-# set FSM class
-def setFSM(lines, fsm):
-    idx = 1
-    stat = 0
-    loopStat = 0
-    beforeStat = 0
-    isVarSetting = 0
-    tempdic = {}
-
-    for line in lines:
-        if line[0] == '$':
-            line = line[1:]
-            words = line.split()
-
-            # read module name and original value names at .vcd and write on the FSM class
-            if words[0] == 'scope':
-                fsm.setModuleName(words[2])
-            elif words[0] == 'var' and words[1] == 'reg':
-                print(words[4], "is assigned to", words[3])
-                fsm.setInputValue(words[3], words[4])
-            else:
-                """nothing"""
-        # set first state(s1) information
-        elif line[0] == '#':
-            if isVarSetting == 1:
-                beforeStat.setTransition(tempdic, "s" + str(idx - 1))
-                fsm.setState(beforeStat)
-                isVarSetting = 0
-            tempdic = {}
-            time = line[1:-1]
-            beforeStat = stat
-            stat = State("s" + str(idx), time)
-
-            idx += 1
-            if time != "0":
-                isVarSetting = 1
-
-        elif line == "...\n":
-            stat.setIsLoop()
-            continue
-        elif line == 'START\n':
-            stat.setStartLabeled()
-        elif line == 'STOP\n':
-            stat.setStopLabeled()
-
-        elif isVarSetting == 1:
-            # go to the next line and set the transitions information
-            value = line[0]
-            var = fsm.getInputVal(line[1])
-            tempdic[var] = value
-
-    beforeStat.setTransition(tempdic, "s" + str(idx - 1))
-    fsm.setState(beforeStat)
-
-    fsm.setState(stat)
-    return fsm
 
 
 if __name__ == "__main__":
@@ -218,7 +212,7 @@ if __name__ == "__main__":
 
     lines = rf.readlines()
     lines = lines[10:]
-    fsm = setFSM(lines, fsm)
+    fsm.setFSM(lines)
     print("FSM created\n")
 
     fsm.printHeader(wf)
@@ -226,4 +220,4 @@ if __name__ == "__main__":
     fsm.printTransition(wf)
 
     rf.close()
-    wf.close()
+wf.close()
